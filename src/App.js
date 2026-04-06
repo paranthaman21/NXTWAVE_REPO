14b9fa9463078b5f9714a55ec5d7b488bb0a4f2e
paranthaman21 <vbarath210706@gmail.com>
2026-04-06 22:51:22 +0530

    Learnt list and arrays in react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
+import './App.css'
 import UserProfile from './components/UserProfile/index'
 
-const userDetail = {
-  imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
-  name: 'Paranthaman',
-  role: 'Full Stack Developer',
-}
+const userDetailsList = [
+  {
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Paranthaman',
+    role: 'Full Stack Developer',
+  },
+  {
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
+    name: 'Floyd Miles',
+    role: 'Software Engineer',
+  },
+  {
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
+    name: 'Jacob Jones',
+    role: 'Software Engineer',
+  },
+  {
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/devon-lane-img.png',
+    name: 'Devon Lane',
+    role: 'Software Engineer',
+  },
+]
 
-const App = () => <UserProfile userDetails={userDetail} />
-
+const App = () => (
+  <div className="list-container">
+    <h1 className="title">Users List</h1>
+    <ul className="users-list">
+      {userDetailsList.map(eachUser => (
+        <UserProfile userDetails={eachUser} key={eachUser.name} />
+      ))}
+    </ul>
+  </div>
+)
 export default App
--- src/components/UserProfile/index.js
+++ src/components/UserProfile/index.js
@@ -1,7 +1,18 @@
-const UserProfile = () => (
-  <div>
-    <img src="https://assets.ccbp.in/frontend/react-js/esther-howard-img.png" />
-  </div>
-)
+import './index.css'
+
+const UserProfile = props => {
+  const {userDetails} = props
+  const {imageUrl, name, role} = userDetails
+
+  return (
+    <li className="user-card-container">
+      <img src={imageUrl} alt={name} className="avatar" />
+      <div className="user-details-container">
+        <h1 className="user-name">{name}</h1>
+        <p className="user-designation">{role}</p>
+      </div>
+    </li>
+  )
+}
 
 export default UserProfile
